25f50245b9e59747884a009890f100973ad67285
Diogo <>
2020-12-12 12:36:46 +0100

    Question suppl

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -1,6 +1,7 @@
 package agenda;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 
@@ -11,6 +12,7 @@ public class Agenda {
     
     public List<Event> myEvents = new ArrayList<>();
     public List<Event> myEventsDay = new ArrayList<>();
+    public List<Event> myEventTitle = new ArrayList<>(); 
     
     /**
      * Adds an event to this agenda
@@ -36,5 +38,34 @@ public class Agenda {
             }
         }
         return myEventsDay;
+    }
+    
+     /**
+     * Trouver les événements de l'agenda en fonction de leur titre
+     * @param title le titre à rechercher
+     * @return les événements qui ont le même titre
+     */
+    public List<Event> findByTitle(String title) {
+        // TODO : implémenter cette méthode
+        for(Event e:myEvents){
+            if(e.getTitle().equals(title)){
+                myEventTitle.add(e);
+            }
+        }
+        return myEventTitle;
+    }
+    
+    /**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) {
+        // TODO : implémenter cette méthode
+        for(Event evenement:myEvents){
+            if(e.getStart().isAfter(evenement.getStart()))
+                return false;
+        }
+        return true;
     }
 }
